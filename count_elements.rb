572dec742ad25a1fa715637096758e929765eb59
Marcus Plenty <marcusplenty@gmail.com>
2018-11-20 00:54:31 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
-  # code goes here
+  new_hash ={}
+  array.each do |animal|
+    if new_hash.has_key?(animal) == false
+      new_hash[animal] = 1
+    else
+      new_hash[animal] += 1
+    end
+  end
+  return new_hash
 end
  
